5adfb241f1265c795766559619a733fab3aacf16
Sebastian Hofer <sebastian.hofer@gmx.net>
2021-09-10 21:13:49 +0200

    Loop over all sounds and create audio elements.

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,51 @@
-<template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+<template lang="html">
+  <div style="height: 100px">
+    <div v-for="(sounds, unit) in sounds" :key="unit" >
+      {{ unit }}
+      <div v-for="(src, sound) in sounds" :key="sound">
+        {{ sound }}
+        <audio controls>
+          <source :src="src" type="audio/wav">
+        </audio>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
-
 export default {
-  name: 'Home',
-  components: {
-    HelloWorld
+  data() {
+    return {
+      sounds: {}
+    }
+  },
+
+  mounted() {
+    this.importAll(require.context('@/assets/en/', true, /^.*\.wav$/))
+  },
+
+  methods: {
+    importAll(r) {
+      for (const assetPath of r.keys()){
+        const unitName = this.getUnitName(assetPath)
+        if (!(unitName in this.sounds)) {
+          this.sounds[unitName] = {}
+        }
+        this.sounds[unitName][this.getSoundName(assetPath)] = r(assetPath)
+      }
+    },
+    getUnitName(path) {
+      // ./wc3sfx-creeps-ogre/OgreYes4.wav -> Ogre
+      const regex = RegExp('-.+-(.*)/')
+      const match = regex.exec(path)[1]
+      return match.charAt(0).toUpperCase() + match.slice(1);
+    },
+    getSoundName(path) {
+      // ./wc3sfx-creeps-ogre/OgreYes4.wav -> OgreYes4
+      const regex = RegExp('.+/(.*)\\.')
+      const match = regex.exec(path)[1]
+      return match
+    }
   }
 }
 </script>
